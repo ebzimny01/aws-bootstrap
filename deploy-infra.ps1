258dcaa68b5381f9a47eab2e7a300df7c95ab0f8
Ed Zimny <ebzimny@gmail.com>
2021-11-09 17:25:45 -0500

    continuing codedeploy changes in script and main.yml

--- deploy-infra.ps1
+++ deploy-infra.ps1
@@ -10,6 +10,13 @@ $env:AWS_ACCOUNT_ID= docker run --rm -it -v $env:userprofile\.aws:/root/.aws sts
   --query "Account" --output text
 $env:CODEPIPELINE_BUCKET=$env:STACK_NAME + '-' + $env:REGION + '-codepipeline-' + $env:AWS_ACCOUNT_ID
 
+# Generate a personal access token with repo and admin:repo_hook
+#    permissions from https://github.com/settings/tokens
+$env:GH_ACCESS_TOKEN= Get-Content .\.github\aws-bootstrap-access-token -Raw
+$env:GH_OWNER= Get-Content .\.github\aws-bootstrap-owner -Raw
+$env:GH_REPO= Get-Content .\.github\aws-bootstrap-repo -Raw
+$env:GH_BRANCH='main'
+
 # Deploys static resources
 Write-Output "`n`n=========== Deploying setup.yml ==========="
 docker run --rm -it -v $env:userprofile\.aws:/root/.aws `
@@ -35,3 +42,8 @@ docker run --rm -it -v $env:userprofile\.aws:/root/.aws `
   --capabilities CAPABILITY_NAMED_IAM `
   --parameter-overrides `
     EC2InstanceType=$env:EC2_INSTANCE_TYPE
+    GitHubOwner=$env:GH_OWNER `
+    GitHubRepo=$env:GH_REPO `
+    GitHubBranch=$env:GH_BRANCH `
+    GitHubPersonalAccessToken=$env:GH_ACCESS_TOKEN `
+    CodePipelineBucket=$env:CODEPIPELINE_BUCKET
